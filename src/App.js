96286cafb1ca0a0eb8a4c9c2e2f8056635c25abb
jzhang0503 <90428413+jzhang0503@users.noreply.github.com>
2023-01-01 02:50:31 -0500

    starting website

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import SelfIntro from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <SelfIntro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/Home.css'
+
+export default function SelfIntro() {
+    return (
+        <div className='home'>
+            <div className='name-intro'>
+                Hi! I'm Junxiang
+                <div className='study-intro'>
+                    I'm a sophomore studying Applied Math-Computer Sciece at Brown University
+                </div>
+            </div>
+
+        </div>
+
+    );
+}
