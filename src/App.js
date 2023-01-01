b47a3e20a050641172660e119962f41e39157b01
jzhang0503 <90428413+jzhang0503@users.noreply.github.com>
2023-01-01 06:02:46 -0500

    working on social links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import SelfIntro from './pages/Home';
+import SocialLinks from './components/Socials';
 
 function App() {
   return (
     <div className="App">
+      
       <SelfIntro/>
+      <SocialLinks/>
     </div>
   );
 }
---
+++ src/components/Socials.js
@@ -0,0 +1,57 @@
+import linkedIn from '../images/linkedin-svgrepo-com.svg'
+import instagram from '../images/iconmonstr-instagram-11.svg'
+import mail from '../images/icons8-mail.svg'
+import '../styles/Socials.css'
+
+function LinkedIn() {
+    return (
+        <div className='linked-in'>
+            <a href='https://www.linkedin.com/in/junxiang-zhang-359918230/' target="_blank">
+                <img
+                    src={linkedIn}
+                >
+                </img>
+            </a>
+        </div>
+
+    );
+}
+function Instagram(){
+    return (
+        <div className='instagram'>
+            <a href='https://www.instagram.com/junxiang_zhang/' target="_blank">
+                <img
+                    src={instagram}
+                >
+                </img>
+            </a>
+        </div>
+
+    );
+}
+
+function Mail(){
+    return (
+        <div className='instagram'>
+            <a href='mailto:junxiang_zhang@brown.edu' target="_blank">
+                <img
+                    src={mail}
+                >
+                </img>
+            </a>
+        </div>
+
+    );
+}
+
+
+export default function SocialLinks() {
+    return (
+    <div className='social-bar'>
+        <Mail/>
+        <LinkedIn/>
+        <Instagram/>
+    </div>);
+
+
+}
